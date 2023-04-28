9281d552747efec52aaa216edb558d95aa6e26d7
Sunil Tadepalli <sunil.tadepalli@qlinics.com>
2023-04-28 12:48:19 +0530

    initial commit

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,17 +1,37 @@
 import 'package:faker_app_flutter_firebase/src/screens/custom_profile_screen.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_sign_in_screen.dart';
+import 'package:faker_app_flutter_firebase/src/screens/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-enum AppRoute {
-  signIn,
-  profile,
-}
+import 'go_router_refresh_stream.dart';
+
+enum AppRoute { signIn, profile, home }
+
+final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
 
 final goRouterProvider = Provider<GoRouter>((ref) {
+  final firebaseAuth = ref.watch(firebaseAuthProvider);
   return GoRouter(
     initialLocation: '/sign-in',
     debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final isLoggedIn = firebaseAuth.currentUser != null;
+      if (isLoggedIn) {
+        if (state.location == '/sign-in') {
+          return '/home';
+        }
+      } else {
+        if (state.location.startsWith('/home')) {
+          return '/sign-in';
+        }
+      }
+      return null;
+    },
+    refreshListenable: GoRouterRefreshStream(firebaseAuth.authStateChanges()),
     routes: [
       GoRoute(
         path: '/sign-in',
@@ -19,9 +39,16 @@ final goRouterProvider = Provider<GoRouter>((ref) {
         builder: (context, state) => const CustomSignInScreen(),
       ),
       GoRoute(
-        path: '/profile',
-        name: AppRoute.profile.name,
-        builder: (context, state) => const CustomProfileScreen(),
+        path: '/home',
+        name: AppRoute.home.name,
+        builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            path: 'profile',
+            name: AppRoute.profile.name,
+            builder: (context, state) => const CustomProfileScreen(),
+          ),
+        ],
       ),
     ],
   );
